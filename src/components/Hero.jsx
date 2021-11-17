d02b0bc4117b3528aa88d12b86a63396dc02a4cb
Christian Ndu <christiannduh@gmail.com>
2021-11-17 20:53:28 +0100

    add favicon and logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 export default function Hero() {
 	return (
 		<div className="p-8 mx-auto flex flex-col items-center ">
-			<h1 className="font-extrabold text-7xl text-blue-600 ">FRONTPAGE </h1>
+			<h1 className="font-extrabold text-7xl text-blue-600 ">FRONTPAGE</h1>
 			<p className="font-bold text-3xl text-blue-500 w-4/5 mx-auto p-2 mt-5">Find the breaking news from multiple news sites on your favorite topics.</p>
 		</div>
 	);
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
+import Logo from "../assets/logo.png"
+
 export default function Navbar() {
 	return (
 		<div className="w-4/5 p-6 rounded-lg mx-5 my-6 flex justify-between items-center text-white bg-blue-600 ">
-			<h1 className="font-logo font-bold mr-3 text-2xl">Frontpage</h1>
+			<div className="w-[250px] h-full pointer">
+			<img src={Logo} alt="logo"/></div>
 			<ul  className="font-semibold flex justify-between items-center ">
 			<li className="mr-3"><a href="https://khrees.netlify.app" target="_blank" rel="noreferrer noopener">About Developer</a> </li>
 
